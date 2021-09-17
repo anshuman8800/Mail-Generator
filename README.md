8b60c6d094ae763e4b6f868c0eef6cbcf58c3067
Anshuman <anshuman8800@gmail.com>
2021-09-17 12:09:17 +0530

    Use SMTP and create CSVfile list

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Mail-Generator
+
+Set up a secure connection using SMTP_SSL() and .starttls()
+
+Use Python’s built-in smtplib library to send basic emails
+
+Send multiple personalized emails using a CSV file with contact data
---
+++ mailGenerator.py
@@ -0,0 +1,28 @@
+import smtplib
+import csv
+password = '****'
+mailID = 'anshuman8800@gmail.com'
+
+
+message = """
+Subject: Regarding auto generated mail
+CC: anshuman19@iiitg.ac.in
+
+This message is sent from Python.
+Thanks with Regard
+Anshuman"""
+
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.ehlo()
+
+  
+s.starttls()
+s.login(mailID, password)  
+with open("contacts_file.csv") as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip header row
+    for name, email in reader:
+        print(f"Sending email to {name}")
+        s.sendmail(mailID, email, message)
+s.quit()
